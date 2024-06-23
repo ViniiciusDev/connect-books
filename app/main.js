c32f628b9708678660900b0c586750781f5ea58a
Vinicius Gomes <ViniiciusGomes27@gmail.com>
2024-06-23 15:27:51 +0200

    Refactor: Create a bookshelf file.js

--- app/main.js
+++ app/main.js
@@ -1,5 +1,4 @@
 let books = [];
-const bookShelf = document.querySelector("#livros");
 const endPointBooksAPI =
     "https://guilhermeonrails.github.io/casadocodigo/livros.json";
 
@@ -10,27 +9,5 @@ async function getBooksFromAPI() {
     console.table(books);
     showBooksInTheBookShelf(books);
 }
-getBooksFromAPI();
 
-// Creating the books elements and put them in the bookshelf
-function showBooksInTheBookShelf(booksList) {
-    books.forEach((book) => {
-        bookShelf.innerHTML += `
-            <div class="livro">
-                <img
-                    class="livro__imagens"
-                    src="${book.imagem}"
-                    alt="${book.alt}"
-                />
-                <h2 class="livro__titulo">
-                    ${book.titulo}    
-                </h2>
-                <p class="livro__descricao">${book.autor}</p>
-                <p class="livro__preco" id="preco">R$${book.preco}</p>
-                <div class="tags">
-                    <span class="tag">${book.categoria}</span>
-                </div>
-            </div>
-        `;
-    });
-}
+getBooksFromAPI();
